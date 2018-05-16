40a244f5eb8e6e128244d19a85812ff76287b31e
malydok <marek@devalchemist.com>
2018-05-16 10:55:32 +0200

    add firebase dependency and setup

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+
+import 'firebase/auth';
+// import 'firebase/database';
+// import 'firebase/firestore';
+// import 'firebase/messaging';
+// import 'firebase/functions';
+
+const config = {
+  apiKey: 'AIzaSyDp27tiKuLQnC3o6ValBUdxhtq1z4nyHIM',
+  authDomain: 'start-bodyweight.firebaseapp.com',
+  databaseURL: 'https://start-bodyweight.firebaseio.com',
+  projectId: 'start-bodyweight',
+  storageBucket: 'start-bodyweight.appspot.com',
+  messagingSenderId: '917856758898'
+};
+firebase.initializeApp(config);
