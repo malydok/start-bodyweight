454b0aacac89412ee384b09df5d7a3e8686608aa
malydok <marek@devalchemist.com>
2018-05-25 10:52:52 +0200

    NavMenu formatting change

--- src/NavMenu.js
+++ src/NavMenu.js
@@ -9,45 +9,43 @@ const { SubMenu } = Menu;
 
 const NavMenu = () => (
   <Route
-    render={({ location }) => {
-      return (
-        <Menu
-          mode="inline"
-          theme="dark"
-          defaultSelectedKeys={[location.pathname]}
+    render={({ location }) => (
+      <Menu
+        mode="inline"
+        theme="dark"
+        defaultSelectedKeys={[location.pathname]}
+      >
+        <Menu.Item key="/">
+          <Link to="/">
+            <Icon type="play-circle-o" />
+            Workout
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="/my-progress">
+          <Link to="/my-progress">
+            <Icon type="schedule" />
+            My progressions
+          </Link>
+        </Menu.Item>
+        <SubMenu
+          key="progression"
+          title={
+            <span>
+              <Icon type="line-chart" />
+              Progressions
+            </span>
+          }
         >
-          <Menu.Item key="/">
-            <Link to="/">
-              <Icon type="play-circle-o" />
-              Workout
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="/my-progress">
-            <Link to="/my-progress">
-              <Icon type="schedule" />
-              My progressions
-            </Link>
-          </Menu.Item>
-          <SubMenu
-            key="progression"
-            title={
-              <span>
-                <Icon type="line-chart" />
-                Progressions
-              </span>
-            }
-          >
-            {Object.keys(excercises).map(type => (
-              <Menu.Item key={`/progression/${type}`}>
-                <Link to={`/progression/${type}`}>
-                  {capitalizeFirstLetter(type)}
-                </Link>
-              </Menu.Item>
-            ))}
-          </SubMenu>
-        </Menu>
-      );
-    }}
+          {Object.keys(excercises).map(type => (
+            <Menu.Item key={`/progression/${type}`}>
+              <Link to={`/progression/${type}`}>
+                {capitalizeFirstLetter(type)}
+              </Link>
+            </Menu.Item>
+          ))}
+        </SubMenu>
+      </Menu>
+    )}
   />
 );
 
